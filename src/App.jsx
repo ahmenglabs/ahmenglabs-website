0f1271a8b5fad85ea33d3aaa21f460aabcae70ae
Andika Dinata <a.notmuehehe@gmail.com>
2025-10-30 19:41:38 +0700

    initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <img src="/src/assets/logo.png" alt="Logo" />
+      <p>Under Development</p>
+    </div>
+  );
+}
+
+export default App;
